f7816dc669019d01682cd3839890351360b8868c
IKTIKN <developermarc@outlook.com>
2024-02-23 02:43:32 +0100

    Rating component

---
+++ src/app/components/rating/rating.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Rating } from 'src/app/interfaces/ReleaseResponse';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.scss'],
+})
+export class RatingComponent  implements OnInit {
+  @Input() rating!: Rating;
+  percentage: number = 0;;
+  
+  constructor() { }
+
+  ngOnInit() {
+    this.percentage = this.rating.average * 20;
+  }
+
+  formatRating(rating: number): number {
+    return Math.round(rating * 100) / 100;
+  }
+  
+}
