161bc59906447f013f28cc73f5478c922b7b3b43
IKTIKN <developermarc@outlook.com>
2024-02-25 23:04:30 +0100

    Release component

---
+++ src/app/components/release/release.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { CommonService } from 'src/app/services/common/common.service';
+import { ReleaseResponse } from 'src/app/interfaces/ReleaseResponse';
+import { CountryService } from 'src/app/services/country/country.service';
+import { CountryFlag } from 'src/app/interfaces/CountryFlag';
+
+@Component({
+  selector: 'app-release',
+  templateUrl: './release.component.html',
+  styleUrls: ['./release.component.scss'],
+})
+export class ReleaseComponent  implements OnInit {
+  @Input() release!: ReleaseResponse;
+  common: CommonService = inject(CommonService);
+  country: CountryService = inject(CountryService);
+  countryFlags!: CountryFlag[];
+
+  constructor() { }
+  
+  ngOnInit() {
+    this.countryFlags = this.country.getCountryFlags(this.release.country);
+  }
+
+}
